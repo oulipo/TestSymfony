c23e04160f11abc75fa36029b50ce8440d640758
oulipo <laudesj@gmail.com>
2018-07-05 08:51:53 +0200

    doctrine 1

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,8 +4,12 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
-use Date;
+use App\Entity\Article;
+
+
 
 class DefaultController extends Controller
 {
@@ -36,6 +40,74 @@ class DefaultController extends Controller
     public function design() {
         return $this->render('default/design.html.twig');
     }
+
+    public function creer_article($titre, $contenu) {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = new Article();
+        $article->setTitre($titre);
+        $article->setContenu($contenu);
+        $article->setCreatedAt(new \DateTime("now"));
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return new Response('Article crée, avec id = ' . $article->getId());
+    }
+
+    public function article($id) {
+        $repository = $this->getDoctrine()->getRepository(Article::class);
+
+        $article = $repository->find($id);
+        return $this->render('default/article.html.twig', ['article' => $article]); 
+    }
+
+    public function articles() {
+        $repository = $this->getDoctrine()->getRepository(Article::class);
+
+        $articles = $repository->findAll();
+        
+        return $this->render('default/articles.html.twig', ['articles' => $articles]); 
+    }
+
+    public function articles_by($prop, $recherche) {
+        $repository = $this->getDoctrine()->getRepository(Article::class);
+
+        $articles = $repository->findBy(
+            [ $prop => '%'.$recherche.'%' ] // TODO: recherche globale 
+            );
+        
+        return $this->render('default/articles.html.twig', ['articles' => $articles]); 
+    }
+
+    public function update($id, $prop, $valeur) {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        switch($prop) {
+            case 'titre':
+                $article->setTitre($valeur);
+            break;
+            case 'contenu':
+                $article->setContenu($valeur);
+            break;
+            default:
+            break;
+        }
+        $entityManager->flush();
+        return $this->redirectToRoute('article', [ 'id' => $article->getId()]);
+        
+    }
+
+    public function supprimer($id) {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        $entityManager->remove($article);
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('articles');
+        
+    }
+
 
 
 }
---
+++ src/Entity/Article.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+}
